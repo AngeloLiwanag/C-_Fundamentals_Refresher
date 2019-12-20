9b660218b3f642030b1cf176b77dc0274af5bfa9
Angelo Liwanag <AngeloLiwanag75@gmail.com>
2019-12-20 14:13:34 -0800

    refresher

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Fundamentals
+{
+    class Program
+    {
+        public static global::System.Object System { get; private set; }
+
+        static void Main(string[] args)
+        {
+            // for(int i = 1; i <= 255; i++)
+            // {
+            //     System.Console.WriteLine(i);
+            // }
+
+            for (int i = 1; i <=100; i++)
+            {
+                if(i % 3 == 0 || i % 5 == 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+
+            // for (int i = 1; i <=100; i++)
+            // {
+            //     if (i % 3 == 0 && i % 5 == 0)
+            //     {
+            //         System.Console.WriteLine("FizzBuzz");
+            //     } else if(i % 3 == 0)
+            //     {
+            //         System.Console.WriteLine("Fizz");
+            //     } else if (i % 5 == 0) 
+            //     {
+            //         System.Console.WriteLine("Buzz");
+            //     } 
+            // }
+            
+        }
+
+    }
+
+}
